9c52d4186875226786c06f6b42e40b18eae1aaad
rofl0r <rofl0r@users.noreply.github.com>
2021-10-20 21:40:40 +0000

    build.sh: warn without opts, don't enforce C99

--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@ if [ -z "$1" ] ; then
 	exit 1
 fi
 
+if test -z "$CC" -o -z "$OPTS" ; then
+	echo "warning: CC or OPTS not set, which is probably not what you want" >&2
+fi
+
 [ -z "$CC" ] && CC=gcc
 $CC -c dummyfuncs.c -o dummyfuncs.o
 $CC -D_GNU_SOURCE -D_BSD_SOURCE -O0 -g3 -DFILENAME=\"$1\" -c memcpy_test.c -o memcpy_test.o
-$CC $OPTS -std=c99 -c "$1" -o mymemcpy.o
+$CC $OPTS -c "$1" -o mymemcpy.o
 $CC dummyfuncs.o memcpy_test.o mymemcpy.o -o test
